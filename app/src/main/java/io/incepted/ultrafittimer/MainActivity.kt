497084742f671e437d3c5ca93753eca2c16e3bd6
Wasabi <hyclairelee@gmail.com>
2018-07-30 21:08:58 -0400

    MainActivity layout

--- app/src/main/java/io/incepted/ultrafittimer/MainActivity.kt
+++ app/src/main/java/io/incepted/ultrafittimer/MainActivity.kt
@@ -2,11 +2,21 @@ package io.incepted.ultrafittimer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
